5281e19936efdd62494c31b464eea29beb65915a
Harkit07 <harkitsinghsran9584@gmail.com>
2025-01-27 15:57:54 +0530

    add files

--- middleware.js
+++ middleware.js
@@ -1,8 +1,3 @@
-// const Listing = require("./models/listing");
-// const Review = require("./models/reviews.js");
-// const ExpressError = require("./utils/ExpressError.js");
-// const { listingSchema, reviewSchema } = require("./schema.js");
-
 module.exports.isLoggedIn = (req, res, next) => {
   res.locals.currUser = req.user;
   if (!req.isAuthenticated()) {
@@ -12,51 +7,3 @@ module.exports.isLoggedIn = (req, res, next) => {
   }
   next();
 };
-
-// module.exports.saveRedirectUrl = (req, res, next) => {
-//   if (req.session.redirectUrl) {
-//     res.locals.redirectUrl = req.session.redirectUrl;
-//   }
-//   next();
-// };
-
-// module.exports.isOwner = async (req, res, next) => {
-//   let { id } = req.params;
-//   let listing = await Listing.findById(id);
-//   if (!listing.owner._id.equals(res.locals.currUser._id)) {
-//     req.flash("error", "You are not the owner of this listings");
-//     return res.redirect(`/listings/${id}`);
-//   }
-//   next();
-// };
-
-// module.exports.validateListing = (req, res, next) => {
-//   let { error } = listingSchema.validate(req.body);
-//   if (error) {
-//     let errMsg = error.details.map((el) => el.message).join(",");
-//     throw new ExpressError(404, errMsg);
-//   } else {
-//     next();
-//   }
-// };
-
-// module.exports.validateReview = (req, res, next) => {
-//   let { error } = reviewSchema.validate(req.body);
-//   console.log(error);
-//   if (error) {
-//     let errMsg = error.details.map((el) => el.message).join(",");
-//     throw new ExpressError(404, errMsg);
-//   } else {
-//     next();
-//   }
-// };
-
-// module.exports.isReviewAuthor = async (req, res, next) => {
-//   let { id, reviewId } = req.params;
-//   let review = await Review.findById(reviewId);
-//   if (!review.author._id.equals(res.locals.currUser._id)) {
-//     req.flash("error", "You are not the author of this review");
-//     return res.redirect(`/listings/${id}`);
-//   }
-//   next();
-// };
